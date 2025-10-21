8a032e47175ea0b1443c796a59341f967e7722f5
Ilya510 <73663891+Ilya510@users.noreply.github.com>
2025-10-21 09:39:54 +0300

    Add files via upload

---
+++ newstonepaper.py
@@ -0,0 +1,16 @@
+pc : str = str('камень')
+
+
+
+
+player : str = str(input('ввод'))
+
+
+if(player=='бумага'):
+    print('player won')
+elif(player=='камень'):
+    print('nobody won')
+else:
+    print('pc won')
+
+
